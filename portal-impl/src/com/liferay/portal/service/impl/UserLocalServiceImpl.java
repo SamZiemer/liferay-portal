d35ae983d718398b2440be81a0dfb6e4fa875f36
Michael Bowerman <michael.bowerman@liferay.com>
2016-04-22 10:58:47 -0700

    LPS-65275 Add property to allow user to enable workflow for users created through the control panel or through LDAP

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -564,7 +564,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		boolean workflowEnabled = WorkflowThreadLocal.isEnabled();
 
 		try {
-			WorkflowThreadLocal.setEnabled(false);
+			if (!PropsValues.USERS_CREATION_ALWAYS_APPLY_WORKFLOW) {
+				WorkflowThreadLocal.setEnabled(false);
+			}
 
 			if (serviceContext == null) {
 				serviceContext = new ServiceContext();
