507fbe1ba704248a0eae11ec0a9cac968fff736c
Spencer <spencer.woo@liferay.com>
2017-06-26 17:29:16 -0700

    LPS-73315 Add passwordRemainingDays

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3017,6 +3017,36 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		return false;
 	}
 
+
+	/**
+	 * Returns the number of days remaining until the user's password is expired.
+	 *
+	 * @param  user the user
+	 * @return the number of days until the user's password expires;
+	 *         -1 when the user's password does not expire.
+	 */
+	public int passwordRemainingDays(User user) throws PortalException {
+		PasswordPolicy passwordPolicy = user.getPasswordPolicy();
+
+		if ((passwordPolicy != null) && passwordPolicy.isExpireable()) {
+			Date now = new Date();
+
+			long timeModified = user.getPasswordModifiedDate().getTime();
+
+			long passwordExpiresOn =
+				(passwordPolicy.getMaxAge() * 1000) + timeModified;
+
+			long dayTime = 24 * 60 * 60 * 1000;
+
+			int remainingDays =
+				(int)((passwordExpiresOn - now.getTime()) / dayTime);
+
+			return (remainingDays > 0) ? remainingDays : 0;
+		}
+
+		return -1;
+	}
+
 	/**
 	 * Returns the default user for the company.
 	 *
