1140af26372481ef140307d9218b4c47debc7f1c
Brian I. Kim <il.kim@liferay.com>
2019-02-11 14:50:49 -0800

    LPS-89922 Implement more button functionality for documents and media

--- modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/js/main.js
+++ modules/apps/document-library/document-library-web/src/main/resources/META-INF/resources/document_library/js/main.js
@@ -34,6 +34,10 @@ AUI.add(
 						validator: Lang.isString
 					},
 
+					openViewMoreFileEntryTypesURL: {
+						validator: Lang.isString
+					},
+
 					searchContainerId: {
 						validator: Lang.isString
 					},
@@ -145,6 +149,22 @@ AUI.add(
 						if (action) {
 							instance._processAction(action, url);
 						}
+					},
+
+					handleCreationMenuMoreButtonClicked: function(event) {
+						var instance = this;
+
+						Liferay.Util.openWindow(
+							{
+								dialog: {
+									destroyOnHide: true,
+									modal: true
+								},
+								id: instance.ns('selectAddMenuItem'),
+								title: Liferay.Language.get('more'),
+								uri: instance.get('openViewMoreFileEntryTypesURL')
+							}
+						);
 					},
 
 					handleFilterItemClicked: function(event) {
