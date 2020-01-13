09c8d4f9f65d37696778fb10e706b41b400a3bae
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2020-01-13 15:31:54 -0800

    LPS-107149 build-service

--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -1021,7 +1021,6 @@ create table Release_ (
 	schemaVersion VARCHAR(75) null,
 	buildNumber INTEGER,
 	buildDate DATE null,
-	verified BOOLEAN,
 	state_ INTEGER,
 	testString VARCHAR(1024) null
 );
